23e1a5c4c8446a0cd0a2fed7bbfe111af5de3a35
Rafael Floriano da Silva <rflorianobr@gmail.com>
2015-05-08 22:17:46 -0300

    0.1.0

--- walking_settings/version.py
+++ walking_settings/version.py
@@ -7,4 +7,4 @@
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/MIT-license
 # Copyright (c) 2015, Rafael Floriano da Silva <rflorianobr@gmail.com>
-__version__ = '0.0.1'  # NOQA
+__version__ = '0.1.0'  # NOQA
